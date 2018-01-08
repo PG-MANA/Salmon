2ffffc80859df7146ed49c83a7a5ecef14ff2393
PG_MANA <pg_mana@pg-mana.net>
2018-01-08 22:14:11 +0900

    [new] rpm作成用のスクリプなどを追加
    バイナリの名前は小文字のほうが扱いやすいと考え変更
    Ver.0.0.2開発開始
    ユーザー詳細表示などを目指す

--- src/Salmon.h
+++ src/Salmon.h
@@ -15,6 +15,6 @@
 #define USER_AGENT           "Salmon for Linux"
 #define APP_NAME             "Salmon"
 #define APP_NAME_LONG   "Salmon for Linux"
-#define APP_VERSION          "0.0.1"
+#define APP_VERSION          "0.0.2 Dev"
 #define APP_HOMEPAGE         "https://mnas.info/soft/salmon/linux/"
 #define APP_COPYRIGHT        "Copyright 2017 PG_MANA"
